4d1e3acb7b8989593f38f69b020c16d9b7163e12
goat2003 <1679369498@qq.com>
2021-05-30 17:41:07 +0800

    Site updated: 2021-05-30 17:41:07

---
+++ js/crash_cheat.js
@@ -0,0 +1,16 @@
+ var OriginTitle = document.title;
+ var titleTime;
+ document.addEventListener('visibilitychange', function () {
+     if (document.hidden) {
+         $('[rel="icon"]').attr('href', "/joke.ico");
+         document.title = '看不见我🙈~看不见我🙈~';
+         clearTimeout(titleTime);
+     }
+     else {
+         $('[rel="icon"]').attr('href', "/favicon.ico");
+         document.title = ' ( ๑•̀ㅂ•́) ✧被发现了～';
+         titleTime = setTimeout(function () {
+             document.title = OriginTitle;
+         }, 2000);
+     }
+ });
